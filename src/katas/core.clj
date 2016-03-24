a2e4637b8b6ec1b6fb1344333a167324cefa65a1
Mark Shvets <mshvets@chariotsolutions.com>
2016-03-24 11:14:57 -0400

    Added an optimal implementation that will exit earlier

--- src/katas/core.clj
+++ src/katas/core.clj
@@ -1,5 +1,19 @@
 (ns katas.core
   (:gen-class))
 
+
 (defn all-unique? [seq]
+  (let [step (fn step [seq-internal seen]
+               (let [first-element (first seq-internal)]
+                 (if (not first-element)
+                   ;exit for recursion we exhausted the seq
+                   true
+                   (if (contains? seen first-element)
+                     ;found a dup we can exit now
+                     false
+                     (step (rest seq-internal) (conj seen first-element)))
+                   )))]
+    (step seq #{})))
+
+(defn all-unique-ez? [seq]
   (= (count seq) (count (distinct seq))))
--- test/katas/core_test.clj
+++ test/katas/core_test.clj
@@ -9,3 +9,11 @@
     (is (= true (all-unique? nil)))
     (is (= false (all-unique? [1 2 3 4 5 4 2])))
     (is (= true (all-unique? '(1 2 3 4 5))))))
+
+(deftest all-unique-ez
+  (testing "FIXME, I fail."
+    (is (= true (all-unique-ez? "Hey pa!")))
+    (is (= false (all-unique-ez? "Hi mom")))
+    (is (= true (all-unique-ez? nil)))
+    (is (= false (all-unique-ez? [1 2 3 4 5 4 2])))
+    (is (= true (all-unique-ez? '(1 2 3 4 5))))))
